23d0429e422f1c29ae144afc0127d4ea6124669e
nanes03 <inesmoalla68@gmail.com>
2024-04-14 15:01:54 +0100

    first commit

--- src/main/java/com/example/demo/Authentication/AuthController.java
+++ src/main/java/com/example/demo/Authentication/AuthController.java
@@ -5,14 +5,11 @@ import com.example.demo.Dto.LoginDto;
 import com.example.demo.Dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-
+@CrossOrigin( origins = "http://localhost:8081")
 public class AuthController {
     private final AuthService authService;
 
---
+++ src/main/java/com/example/demo/Config/Configuration.java
@@ -0,0 +1,17 @@
+package com.example.demo.Config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class Configuration implements WebMvcConfigurer {
+@Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+    corsRegistry.addMapping("/**")
+            .allowedOrigins("http://localhost:8081")
+            .allowedMethods("GET" ,"POST" ,"PUT" ,"DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+}
+
+
+}
